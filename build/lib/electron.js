f2619b62ced441e336960360892e1cff9a2c54b7
Deepak Mohan <hop2deep@gmail.com>
2020-06-12 21:22:06 -0700

    temp: verifying node fix

--- build/lib/electron.js
+++ build/lib/electron.js
@@ -76,7 +76,7 @@ exports.config = {
     linuxExecutableName: product.applicationName,
     winIcon: 'resources/win32/code.ico',
     token: process.env['VSCODE_MIXIN_PASSWORD'] || process.env['GITHUB_TOKEN'] || undefined,
-    repo: product.electronRepository || undefined
+    repo: 'microsoft/vscode-electron-prebuilt'
 };
 function getElectron(arch) {
     return () => {
